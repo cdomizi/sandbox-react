aab386e2239213d45e0de7f850e520bc529ec384
Cesare Domizi <domizicesare@gmail.com>
2023-01-16 17:51:00 +0100

    refactor navbar and drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import OldNavbar from './OldNavbar/Navbar';
-import Navbar from './Navbar/Navbar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+
+//projects imports
+import Header from './Header/Header';
+import ResponsiveDrawer from './Navbar/ResponsiveDrawer';
+import MainLayout from './MainLayout/MainLayout';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <OldNavbar />
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Header />
+      <ResponsiveDrawer />
+      <MainLayout />
+    </Box>
     </div>
   );
 }
---
+++ src/Navbar/ResponsiveDrawer.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+
+// project imports
+import Navbar from './Navbar';
+
+const drawerWidth = 240;
+
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Drawer
+        container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Navbar />
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        <Navbar />
+      </Drawer>
+    </Box>
+  );
+}
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
