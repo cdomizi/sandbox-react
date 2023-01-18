33525a7b5b8d738444ec8b68def5a1630eddfaca
Cesare Domizi <domizicesare@gmail.com>
2023-01-18 17:57:19 +0100

    add Counter component

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -3,10 +3,20 @@ import { useState } from "react";
 function Counter() {
   const [ count, setCount ] = useState(0);
 
+  function subtract() {
+    setCount(count => count - 1)
+  }
+  function add() {
+    setCount(count => count + 1)
+  }
+
   return (
-    <div>
+    <>
       <h1>Counter</h1>
-    </div>
+      <button onClick={subtract}>-</button>
+      <button onClick={add}>+</button>
+      <p>{count}</p>
+    </>
   );
 }
 
