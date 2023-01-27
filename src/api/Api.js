b45541ac681577eb31690e4077f6037665de3164
Cesare Domizi <domizicesare@gmail.com>
2023-01-27 15:50:33 +0100

    fetch and conditionally display data, manage loading and errors

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,54 @@
+import { useState, useEffect } from "react";
+
 function Api() {
+  const [ data, setData ] = useState(null);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://api.npoint.io/497358e0bb943bdbb3dc');
+        const json = await res.json();
+  
+        !ignore && setData(json);
+        return ignore = true;
+      } catch(e) {
+        !ignore && setData(false)
+        return ignore = true;
+      }
+    };
+
+    setTimeout(() => {
+      fetchData()
+    }, 2000);
+    
+    return () => ignore = true;
+  }, [data]);
+
+  const log = (arg) => {
+    return (
+      <>
+        <pre>
+          <code>
+            {JSON.stringify(arg, null, 2)}
+          </code>
+        </pre>
+      </>
+    );
+  };
+
   return (
-    0
+    <>
+      { data === null ?
+        'Sto caricando....' :
+        !data ?
+          'Errore: impossibile caricare i dati' :
+          !data.compensi.length ?
+            'Nessun risultato trovato' :
+            log(data.compensi)
+      }
+    </>
   );
 }
 
